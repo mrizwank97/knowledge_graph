9a6d36442b0f8cb15ad08dd249dddf48fce5a6bb
Muhammad Rizwan Khalid <rizwan@macbook-air.home>
2023-05-03 01:44:44 +0200

    ABOX done, need to debug for exceptions

--- src/main/java/com/sdm/rdfs/Commons/Constants.java
+++ src/main/java/com/sdm/rdfs/Commons/Constants.java
@@ -2,9 +2,9 @@ package com.sdm.rdfs.Commons;
 
 public class Constants {
 
-    public static final String BASE_URI = "http://www.sdm.upc.es/lab3/AliakberovaKhalid/ontology/TBOX/rdf-schema";
+    public static final String BASE_URI = "http://www.sdm.upc.es/#";
     public static final String TBOX_MODEL_PATH = "data/ontology.rdf";
     public static final String ABOX_MODEL_PATH = "data/publications_data.nt";
-    public static final String DATA_FILE_PATH = "data/raw/instances_data.csv";
+    public static final String DATA_FILE_PATH = "data/rdf_instances.csv";
 
 }
--- src/main/java/com/sdm/rdfs/Commons/Utils.java
+++ src/main/java/com/sdm/rdfs/Commons/Utils.java
@@ -2,50 +2,11 @@ package com.sdm.rdfs.Commons;
 
 public class Utils {
 
-    public static String str_clean(String str) {
+    public static String clean_str_buffer(String str) {
         return str
-                .replace("; ", ";")
-                .replace(".", "_")
-                .replace(",", "_")
-                .replace(": ", ":")
-                .replace("'s", "_s")
-                .replace(" ", "_")
-                .replace("-", "_")
-                .replace("’", "")
-                .replace("©", "")
-                .replace("›", "")
-                .replace("‹", "")
-                .replace("°", "")
-                .replace("%", "")
-                .replace("$", "")
-                .replace("@", "")
-                .replace("!", "")
-                .replace("&", "")
-                .replace("~", "")
-                .replace("'", "")
-                .replace("+", "")
-                .replace("ν", "")
-                .replace("η", "")
-                .replace("?", "")
-                .replace(">", "")
-                .replace("<", "")
-                .replace("μ", "")
-                .replace("\"", "")
-                .replace("\\", "")
-                .replace("^", "")
-                .replace("#", "")
+                .strip()
+                .toLowerCase()
                 .replaceAll("[\\p{Ps}\\p{Pe}]", "") // To remove all opening & closing brackets (https://stackoverflow.com/a/25853119/6390175)
-                .replaceAll("[^A-Za-z0-9] ","")
                 ;
     }
-
-    public static void print(Object text) { System.out.println(text); }
-
-    public static void log(String text) { print("[ log ] " + text); }
-
-    public static void error(String text) { print("[ error ] " + text); System.exit(1); }
-
-    public static void line_separator() { print("\n----------------------------------\n"); }
-
-
 }
