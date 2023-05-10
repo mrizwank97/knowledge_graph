e8c4bb793a21bb470f3cfd350cb51123d738e724
Muhammad Rizwan Khalid <rizwan@macbook-air.home>
2023-05-10 12:20:32 +0200

    TBOX will be created by code now

--- src/main/java/com/sdm/rdfs/Main.java
+++ src/main/java/com/sdm/rdfs/Main.java
@@ -2,6 +2,7 @@ package com.sdm.rdfs;
 
 import java.lang.Exception;
 import com.sdm.rdfs.Ontology.ABOX;
+import com.sdm.rdfs.Ontology.TBOX;
 
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-
+            TBOX.createOntlogyTBOX();
             ABOX.buildAndSaveABOX();
             System.out.println("Model Saved Successfully");
 
